cc302eab6d984e3c29d5cee4099c4815d5c58b75
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-03-18 16:33:04 +0200

    Update api-service.js

--- src/components/api-service.js
+++ src/components/api-service.js
@@ -8,10 +8,20 @@ export default class ImagesApiService {
 
   async fetchImages() {
     const BASE_URL = 'https://pixabay.com/api/';
-    const KEY = '33729128-9c007b81ac695147ac964a843';
-    const url = `${BASE_URL}?key=${KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`;
+    const options = {
+      params: {
+        key: '33729128-9c007b81ac695147ac964a843',
+        q: this.searchQuery,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+        page: this.page,
+        per_page: 40,
+      },
+    };
+    const url = `${BASE_URL}`;
     try {
-      const response = await axios.get(url);
+      const response = await axios.get(url, options);
       this.page += 1;
       console.log(response.data);
       return response;
