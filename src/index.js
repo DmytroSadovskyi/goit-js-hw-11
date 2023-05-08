40734acff12eb121b472a20ed906b221c1f6820b
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-05-08 19:33:03 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import ImagesApiService from './components/api-service';
 import LoadMoreBtn from './components/load-more-btn';
 import createImageCardMarkup from './components/createImageCardMarkup';
-import * as darkMode from './components/darkMode.js';
+
 const refs = {
   searchForm: document.getElementById('search-form'),
   gallery: document.querySelector('.gallery'),
