b86d06b4292ce7549483eb2b364b8b289812523a
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-05-08 19:27:41 +0300

    Update darkmode.js

--- src/components/darkmode.js
+++ src/components/darkmode.js
@@ -3,13 +3,11 @@ const darkModeToggle = document.querySelector('.checkbox');
 let darkMode = localStorage.getItem('darkMode');
 
 const enableDarkMode = () => {
-  darkModeToggle.setAttribute('checked', true);
   html.classList.add('dark-mode');
   localStorage.setItem('darkMode', 'enabled');
 };
 
 const disableDarkMode = () => {
-  darkModeToggle.setAttribute('checked', false);
   html.classList.remove('dark-mode');
   localStorage.setItem('darkMode', null);
 };
